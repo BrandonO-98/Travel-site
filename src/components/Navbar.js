f8e6b0b784c50d4349456eb4e5d09cffb06f34ea
BrandonO-98 <brandonshew98@gmail.com>
2022-04-21 20:30:21 -0400

    Gather images and videos to be used in website and import required packages. Start creating navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,18 @@
+import React, {useState} from "react";
+import {Link} from "react-router-dom";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faCoffee} from "@fortawesome/free-solid-svg-icons";
+
+function Navbar() {
+  return (
+    <nav className='navbar'>
+      <div className='navbar-container'>
+        <Link to='/' className='navbar-logo'>
+          Kazoo <FontAwesomeIcon icon={faCoffee} />
+        </Link>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
