8745c0226cc60da598a69fc3db3dd36a16da9a5e
Brandon Shew <brandonshew@Brandons-MacBook-Pro.local>
2022-04-25 21:33:50 -0400

    Use children as props instead of regular passing of props

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,29 +1,22 @@
 import React from "react";
 import Button from "./Button";
 import "./Hero.css";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faCirclePlay} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
 
 function Hero() {
   return (
-    <div className='hero-container'>
-      <video src='/videos/video-1.mp4' autoPlay loop muted></video>
+    <div className="hero-container">
+      <video src="/videos/video-1.mp4" autoPlay loop muted></video>
       <h1>Adventure Awaits</h1>
       <p>What are you waiting for?</p>
-      <div className='hero-btns'>
-        <Button
-          className='btns'
-          buttonStyle='btn--s'
-          buttonSize='btn--lar'
-          input='Get Started'
-        />
-        <Button
-          className='btns'
-          buttonStyle='btn--p'
-          buttonSize='btn--lar'
-          input='Watch Trailer'
-          inputIcon={<FontAwesomeIcon icon={faCirclePlay} />}
-        />
+      <div className="hero-btns">
+        <Button className="btns" buttonStyle="btn--s" buttonSize="btn--lar">
+          Get Started
+        </Button>
+        <Button className="btns" buttonStyle="btn--p" buttonSize="btn--lar">
+          Watch Trailer <FontAwesomeIcon icon={faCirclePlay} />{" "}
+        </Button>
       </div>
     </div>
   );
